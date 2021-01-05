673b6312327a83be3ccdba998ad79af837097eb3
Saad <saad_shaikh@yahoo.com>
2021-01-04 23:04:02 -0500

    basic redux setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+// React router
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+import App from "./App";
+
+// Redux
+import { Provider } from "react-redux";
+import configureStore from "./store";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+	window.store = store;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
