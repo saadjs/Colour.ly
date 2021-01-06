bf434e7197770594da0c5abf5886a8d15f005fb9
Saad <saad_shaikh@yahoo.com>
2021-01-05 20:01:44 -0500

    added a new nav and nav styling

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -34,19 +34,56 @@ function Nav() {
 	} else {
 		sessionLinks = (
 			<>
-				<button onClick={handleLogout}>Log out</button>
+				<li>
+					<button onClick={handleLogout}>Log out</button>
+				</li>
 			</>
 		);
 	}
 
 	return (
-		<div>
+		<StyledNav>
 			<h1>
 				<NavLink to="/">Home</NavLink>
 			</h1>
 			<ul>{sessionLinks}</ul>
-		</div>
+		</StyledNav>
 	);
 }
+
+const StyledNav = styled.nav`
+	min-height: 10vh;
+	display: flex;
+	margin: auto;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem 10rem;
+	background-color: #1d3557;
+
+	a {
+		color: #f1faee;
+		text-decoration: none;
+	}
+	ul {
+		display: flex;
+		list-style: none;
+	}
+	li {
+		padding-left: 3rem;
+		position: relative;
+	}
+	@media (max-width: 1300px) {
+		flex-direction: column;
+		padding: 2rem 1rem;
+		ul {
+			padding: 2rem;
+			justify-content: space-around;
+			width: 100%;
+			li {
+				padding: 0;
+			}
+		}
+	}
+`;
 
 export default Nav;
