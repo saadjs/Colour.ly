b819395bd2a1bbcda94c2e415622be15b8c45bcb
Saad <saad_shaikh@yahoo.com>
2021-01-10 13:13:43 -0500

    styled the user profile page

--- frontend/src/components/Profile/User.js
+++ frontend/src/components/Profile/User.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import axios from "axios";
+import ColorColumns from "../Home/ColorColumns";
+import styled from "styled-components";
 
 function User({ sessionUser }) {
 	const [userPalettes, getUserPalettes] = useState([]);
@@ -22,14 +24,59 @@ function User({ sessionUser }) {
 	if (!sessionUser) return <Redirect to="/login" />;
 
 	return (
-		<ul>
-			<li>
-				<strong>Username</strong> {user.username}
-			</li>
-			<li>
-				<strong>Email</strong> {user.email}
-			</li>
-		</ul>
+		<MainContainer>
+			<ul>
+				<li>
+					{/* <strong>Username:</strong>  */}
+					<span>{user.username}</span>
+				</li>
+				<li>
+					<strong>Email:</strong> <span>{user.email}</span>
+				</li>
+			</ul>
+			<PaletteContainer>
+				{userPalettes.map((palette) => (
+					<ColorColumns
+						colors={palette.colors}
+						paletteTitle={palette.paletteTitle}
+						paletteId={palette.paletteId}
+					/>
+				))}
+			</PaletteContainer>
+		</MainContainer>
 	);
 }
+
+const MainContainer = styled.div`
+	width: 100%;
+	margin: auto;
+	padding: 0 2rem;
+	border: 3px solid black;
+	ul {
+		list-style-type: none;
+		box-shadow: 10px 5px 5px #e12200;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		padding: 1rem;
+		width: 50%;
+		/* margin: 0 12rem; */
+		margin: auto;
+		li {
+			/* padding: 0.5rem; */
+			font-family: cursive;
+		}
+		span {
+			font-size: 1.5rem;
+		}
+	}
+`;
+
+const PaletteContainer = styled.div`
+	margin: auto;
+	padding: 0 1rem;
+	display: grid;
+	width: 50%;
+`;
+
 export default User;
