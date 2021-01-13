41dd6b0b10ad197abcf1b4f15daadf06e4722f5c
Saad <saad_shaikh@yahoo.com>
2021-01-13 01:07:17 -0500

    added user profile, need backend work

--- frontend/src/components/Profile/User.js
+++ frontend/src/components/Profile/User.js
@@ -5,6 +5,7 @@ import ColorColumns from "../Home/ColorColumns";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import avatar from "./../../styles/images/avatar.jpeg";
 
 function User({ sessionUser, setGetNew }) {
 	const [userPalettes, getUserPalettes] = useState([]);
@@ -35,15 +36,37 @@ function User({ sessionUser, setGetNew }) {
 
 	return (
 		<MainContainer>
-			<ul>
-				<li>
-					<span>{user.username}</span>
-				</li>
-				<li>
-					<strong>Email:</strong> <span>{user.email}</span>
-				</li>
-			</ul>
+			<UserInfoContainer>
+				<ul>
+					<li className="avatar-container">
+						<img src={avatar} alt="avatar" />
+					</li>
+					<li className="username-container">
+						<span>{user.username}</span>
+					</li>
+					<li>
+						<strong>Email:</strong>
+						<a href={`mailto:${user.email}`}>
+							{" "}
+							<span>{user.email}</span>
+						</a>
+					</li>
+					<li>
+						<strong>About Me:</strong>
+						<br />
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						<br /> sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua.
+						<br />
+						Ut enim ad minim veniam, quis nostrud exercitation
+						ullamco laboris
+						<br />
+						nisi ut aliquip ex ea commodo consequat.
+					</li>
+				</ul>
+			</UserInfoContainer>
 			<PaletteContainer>
+				<h1>My Palettes</h1>
 				{userPalettes.map((palette) => (
 					<div key={palette.paletteId} className="palette-delete-div">
 						<ColumnsContainerDiv>
@@ -78,27 +101,47 @@ const MainContainer = styled.div`
 	width: 100%;
 	margin: auto;
 	padding: 0 2rem;
+`;
+const ColumnsContainerDiv = styled.div`
+	flex-grow: 2;
+`;
+const UserInfoContainer = styled.div`
+	position: absolute;
+	top: 10%;
+	width: 20%;
+	.username-container {
+		width: 100%;
+		text-align: center;
+	}
+	.avatar-container {
+		width: 100%;
+	}
+	strong {
+		text-decoration: underline;
+		color: black;
+	}
 	ul {
 		list-style-type: none;
-		box-shadow: 10px 5px 5px #e88a79;
 		display: flex;
 		flex-direction: column;
 		align-items: flex-start;
 		padding: 1rem;
-		width: 50%;
-		margin: auto;
 		li {
 			font-family: cursive;
+			color: #706fd3;
+			img {
+				width: 100%;
+				height: 100%;
+				display: flex;
+				align-items: center;
+				border-radius: 50%;
+			}
 		}
 		span {
 			font-size: 1.5rem;
 		}
 	}
 `;
-const ColumnsContainerDiv = styled.div`
-	flex-grow: 2;
-`;
-
 const PaletteContainer = styled.div`
 	margin: auto;
 	padding: 0 1rem;
@@ -108,6 +151,10 @@ const PaletteContainer = styled.div`
 		margin: 3px 0;
 		display: flex;
 		justify-content: center;
+	}
+	h1 {
+		border-bottom: 2px solid black;
+		text-align: center;
 	}
 `;
 
