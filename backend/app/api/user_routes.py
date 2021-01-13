1a71bcca260f60dc6fd3ef66a3033f88e19211ce
Saad <saad_shaikh@yahoo.com>
2021-01-13 11:49:29 -0500

    remove unnecessary imports and code;

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, request
-from flask.wrappers import Request
 from flask_login import login_required
 from app.models import User, Palette, db
 
@@ -20,7 +19,6 @@ def user(id):
     user = User.query.get(id)
     if not user:
         return jsonify('no user found')
-    # palettes = Palette.query.join(User, Palette.user_id == User.id).filter(User.id == id)
     palettes = Palette.query.filter(Palette.user_id == id).order_by(Palette.id.desc())
     return {'user': user.to_dict(),
             'palettes': [palette.to_dict() for palette in palettes]}
