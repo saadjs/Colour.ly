fc457cb973e14a39ca8637d1349ddd13f44cf8ec
Saad <saad_shaikh@yahoo.com>
2021-01-14 13:23:02 -0500

    added avatar column to user table and more seed data

--- backend/app/seeds/users.py
+++ backend/app/seeds/users.py
@@ -4,11 +4,17 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
-    demo = User(username='Demo', email='demo@aa.io', password='password')
-    saad = User(username='saad', email='saad@saad.com', password='password')
-    bob = User(username='bob', email='bob@bob.com', password='password')
-    cow = User(username='cow', email='cow@wow.com', password='password')
-    monkey = User(username='monkey', email='monkey@monkey.com', password='password')
+    demo = User(username='Demo', email='demo@aa.io', password='password',
+                avatar_url='https://drive.google.com/file/d/1lzgebp0VatsKd1tp41QA-AgtWSnWhL93/view?usp=sharing',
+                about_me='If I lived in a cupboard under the stairs like Harry Potter, I’d still make room for you in my life.')
+    saad = User(username='saad', email='saad@saad.com', password='password',
+                avatar_url='https://drive.google.com/file/d/1vwOEqearXr36ha2IUzqv2FnZZEe41VlB/view?usp=sharing', about_me='If I had a star for every time you brightened my world I’d be holding the entire galaxy in my hands.')
+    bob = User(username='bob', email='bob@bob.com', password='password',
+               avatar_url='https://drive.google.com/file/d/1lzgebp0VatsKd1tp41QA-AgtWSnWhL93/view?usp=sharing', about_me='I think you’re a Pokemon trainer because you just Weedle-d your way into my heart.')
+    cow = User(username='cow', email='cow@wow.com', password='password',
+               avatar_url='https://drive.google.com/file/d/11zIeDSp3TpXgvd1kPJk7vBSU_gwtaUQ4/view?usp=sharing', about_me='You must be an appendix because I don’t know what you do, but I really want to take you out.')
+    monkey = User(username='monkey', email='monkey@monkey.com', password='password',
+                  avatar_url='https://drive.google.com/file/d/1suyjeTkmhzDZZYmKe9BAIwmFOX9mIBga/view?usp=sharing', about_me='Who doesn’t love a totally strange message from a stranger about something completely random?')
 
     db.session.add(demo)
     db.session.add(saad)
