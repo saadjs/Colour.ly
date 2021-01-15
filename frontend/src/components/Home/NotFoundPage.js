ee5c8b2208ea77400a0f653b1c3e60be8b4bb8b0
Saad <saad_shaikh@yahoo.com>
2021-01-15 16:48:57 -0500

    update 404

--- frontend/src/components/Home/NotFoundPage.js
+++ frontend/src/components/Home/NotFoundPage.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { pageAnimation } from "../../styles/Animation";
 import giphy from "./../../styles/images/giphy.gif";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 function NotFoundPage() {
 	return (
@@ -13,8 +15,9 @@ function NotFoundPage() {
 			animate="show"
 			exit="exit"
 		>
+			<FontAwesomeIcon icon={faHome} size="4x" />
 			<h1 style={{ textAlign: "center" }}>
-				<Link to="/">Back Home</Link>
+				<Link to="/">Back to Homepage</Link>
 			</h1>
 			<img src={giphy} alt="giphy" />
 			<p>
@@ -31,7 +34,7 @@ const Error = styled(motion.div)`
 	flex-direction: column;
 	h1 {
 		padding: 1rem;
-		font-size: 5rem;
+		font-size: 4rem;
 	}
 `;
 
