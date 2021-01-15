b779561e7320ffe3f7271f72b113f48ae4a36cb0
Saad <saad_shaikh@yahoo.com>
2021-01-15 18:05:23 -0500

    trying different page transitions

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink, useHistory, useLocation } from "react-router-dom";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
