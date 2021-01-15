76602a50d547d1ad255e9d796e8133f3d179a651
Saad <saad_shaikh@yahoo.com>
2021-01-15 00:25:43 -0500

    home page changes

--- frontend/src/components/Home/DefaultHome.js
+++ frontend/src/components/Home/DefaultHome.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Masony from "react-masonry-component";
 import { motion } from "framer-motion";
+import "./Home.css";
 import { pageAnimation } from "../../styles/Animation";
+import { PHOTOS } from "./util";
+
+const masonryOptions = {
+	fitWidth: false,
+	columnWidth: 300,
+	gutter: 1,
+	itemSelector: ".photo-item",
+};
 
 function DefaultHome() {
 	return (
@@ -11,7 +20,30 @@ function DefaultHome() {
 			animate="show"
 			exit="exit"
 		>
-			<h1>Hello Mate!</h1>
+			<h1 style={{ textAlign: "center" }}>Hello Mate</h1>
+			<Masony
+				className={"photo-list"}
+				elementType={"ul"}
+				options={masonryOptions}
+				disableImagesLoaded={false}
+				updateOnEachImageLoad={false}
+			>
+				{PHOTOS.map((photo, i) => (
+					<motion.li
+						className={`photo-item`}
+						key={i}
+						initial={{ scale: 0 }}
+						animate={{ rotate: 360, scale: 1 }}
+						transition={{
+							type: "spring",
+							// stiffness: 20,
+							damping: 50,
+						}}
+					>
+						<img src={photo.imageUrl} alt="pic" />
+					</motion.li>
+				))}
+			</Masony>
 		</motion.div>
 	);
 }
---
+++ frontend/src/components/Home/util.js
@@ -0,0 +1,138 @@
+import shot1 from "./screenshots/shot1.png";
+import shot2 from "./screenshots/shot2.png";
+import shot3 from "./screenshots/shot3.png";
+import shot4 from "./screenshots/shot4.png";
+import shot5 from "./screenshots/shot5.png";
+import shot6 from "./screenshots/shot6.png";
+import shot7 from "./screenshots/shot7.png";
+import shot8 from "./screenshots/shot8.png";
+import shot9 from "./screenshots/shot9.png";
+import shot10 from "./screenshots/shot10.png";
+import shot11 from "./screenshots/shot11.png";
+import shot12 from "./screenshots/shot12.png";
+import shot13 from "./screenshots/shot13.png";
+import shot14 from "./screenshots/shot14.png";
+import shot15 from "./screenshots/shot15.png";
+import shot16 from "./screenshots/shot16.png";
+import shot17 from "./screenshots/shot17.png";
+import shot18 from "./screenshots/shot18.png";
+import shot19 from "./screenshots/shot10.png";
+import shot20 from "./screenshots/shot20.png";
+import shot21 from "./screenshots/shot21.png";
+import shot23 from "./screenshots/shot23.png";
+import shot24 from "./screenshots/shot24.png";
+import shot25 from "./screenshots/shot25.png";
+import shot26 from "./screenshots/shot26.png";
+import shot27 from "./screenshots/shot27.png";
+import angryPuppy from "./../../styles/images/angryPuppy.jpg";
+import excitedMonkey from "./../../styles/images/excitedMonkey.jpg";
+import hooterWink from "./../../styles/images/hooterWink.jpg";
+import mindBlown from "./../../styles/images/mindBlown.jpg";
+import roboHeart from "./../../styles/images/roboHeart.jpg";
+import skullWinking from "./../../styles/images/skullWinking.jpg";
+import iAmWinking from "./../../styles/images/avatar.jpeg";
+
+export const PHOTOS = [
+	{
+		imageUrl: hooterWink,
+	},
+	{
+		imageUrl: shot1,
+	},
+	{
+		imageUrl: shot2,
+	},
+	{
+		imageUrl: shot3,
+	},
+	{
+		imageUrl: shot4,
+	},
+	{
+		imageUrl: shot5,
+	},
+	{
+		imageUrl: shot6,
+	},
+	{
+		imageUrl: shot7,
+	},
+
+	{
+		imageUrl: roboHeart,
+	},
+	{
+		imageUrl: shot8,
+	},
+	{
+		imageUrl: shot9,
+	},
+	{
+		imageUrl: shot10,
+	},
+	{
+		imageUrl: shot11,
+	},
+
+	{
+		imageUrl: angryPuppy,
+	},
+	{
+		imageUrl: shot12,
+	},
+	{
+		imageUrl: shot13,
+	},
+	{
+		imageUrl: shot14,
+	},
+	{
+		imageUrl: shot27,
+	},
+	{
+		imageUrl: shot15,
+	},
+	{
+		imageUrl: shot16,
+	},
+	{
+		imageUrl: skullWinking,
+	},
+	{
+		imageUrl: shot17,
+	},
+	{
+		imageUrl: shot18,
+	},
+	{
+		imageUrl: iAmWinking,
+	},
+	{
+		imageUrl: shot19,
+	},
+
+	{
+		imageUrl: shot20,
+	},
+	{
+		imageUrl: shot21,
+	},
+	{
+		imageUrl: shot23,
+	},
+	{
+		imageUrl: shot24,
+	},
+	{
+		imageUrl: shot25,
+	},
+	{
+		imageUrl: shot26,
+	},
+	{
+		imageUrl: excitedMonkey,
+	},
+	{
+		imageUrl: mindBlown,
+	},
+];
