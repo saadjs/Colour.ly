feb01ddbcc06f1e7f8b07b16662da9d62095d785
Saad <saad_shaikh@yahoo.com>
2021-01-16 14:29:28 -0500

    add search feature front and back

--- backend/app/api/palette_routes.py
+++ backend/app/api/palette_routes.py
@@ -75,3 +75,14 @@ def post_comment(id):
     db.session.add(new_comment)
     db.session.commit()
     return jsonify(palette.to_dict())
+
+
+# * trying out search feature
+@palette_routes.route('/search', methods=['POST'])
+def search_palettes():
+    tag = request.json['data']
+    search = "%{}%".format(tag)
+    palettes = Palette.query.filter(Palette.title.ilike(search)).all()
+    users = User.query.filter(User.username.ilike(search)).all()
+    return {"users" : [user.to_dict() for user in users],
+            "palettes":[palette.to_dict_search() for palette in palettes]}
