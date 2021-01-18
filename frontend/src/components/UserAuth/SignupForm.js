ade1f73312dd7927e6bd8cd4b0c0c79b958a93f5
saad <saad_shaikh@yahoo.com>
2021-01-17 20:27:10 -0500

    catch phrase removed from login + signup page

--- frontend/src/components/UserAuth/SignupForm.js
+++ frontend/src/components/UserAuth/SignupForm.js
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 //? page transition stuff
 import { motion } from "framer-motion";
-import { signupContainer, titleAnim } from "../styles/Animation";
+import { signupContainer } from "../styles/Animation";
 
 function SignupFormPage({ sessionUser }) {
 	//* redux
@@ -103,12 +103,6 @@ function SignupFormPage({ sessionUser }) {
 					</NavLink>
 				</p>
 			</form>
-			<motion.div className="catch-phrase" variants={titleAnim}>
-				<p>The best colour in the world is your favorite one!!!</p>
-				<p>
-					Let's find that..... <span>Shall We?</span>
-				</p>
-			</motion.div>
 		</motion.div>
 	);
 }
